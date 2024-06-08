fa46e9f59d34894002514f33a78f8326bb2dfd44
Ndah Enkunkwa <anyankrudu@gmail.com>
2024-06-08 18:16:23 +0300

    Working with Javascript

--- script.js
+++ script.js
@@ -55,12 +55,12 @@ const accounts = [account1, account2];
 const btnSignin = document.querySelector(".btn_signin");
 const inputSigninPin = document.querySelector(".form_input--pin");
 const inputSigninUsername = document.querySelector(".form_input--user");
-const containerTransactions = document.querySelector(".all_transactions");
+const containerTransactions = document.querySelector("tbody");
 
 const createUsernames = function (acts, num) {
-  acts.forEach(function (acc, i, end = "...") {
-    let firstSpace = acc.owner.slice(0, num - end.length).indexOf(" ");
-    acc.username = acc.owner
+  acts.forEach(function (act, i, end = "...") {
+    let firstSpace = act.owner.slice(0, num - end.length).indexOf(" ");
+    act.username = act.owner
       .toLowerCase()
       .slice(0, firstSpace > 0 ? firstSpace : num - end.length + 1);
   });
@@ -68,46 +68,43 @@ const createUsernames = function (acts, num) {
 createUsernames(accounts, 10);
 
 const getAllTransactions = function (acts) {
-  acts.forEach(function (acc) {
-    acc.allTrans = Object.entries(acc).filter((val, i, arr) => {
-      return arr[i][0].includes("transaction");
+  acts.forEach(function (act) {
+    act.allTrans = Object.entries(act).filter((tran, i, acc) => {
+      return acc[i][0].includes("transaction");
     });
   });
 };
 getAllTransactions(accounts);
 
-console.log(account1.allTrans);
-
 const displayAccountTransactions = function (act) {
   containerTransactions.innerHTML = " ";
-  act.allTrans.forEach(function (val, i, acc) {
-    let trans = acc[i].find(function (val, i, ac) {
-      return typeof ac[i] === "object";
+
+  act.allTrans.forEach(function (tran, i, acc) {
+    let trans = acc[i].find(function (val, i, acc) {
+      return typeof acc[i] === "object";
     });
 
     let details = trans.details;
-    console.log(details);
     let amt = trans.amount;
-    console.log(amt);
-    let type = amt > 0 ? "income" : "expense";
-    console.log(type);
-    let status = type === "income" ? "complete" : "Incomplete";
-    console.log(status);
+    let type = amt > 0 ? "Income" : "Expense";
+    let status = type === "Income" ? "Completed" : "Incomplete";
 
-    const html = `    <div class="all_transactions">
-  <tr id="all_transactions--row1" class="all_transactions--row2">
+    const html = `    
+   <tr id="all_transactions" class="all_transactions--row">
     <td class="all_transactions_type">${type}</td>
     <td class="all_transactions--details">${details}</td>
     <td class="all_transactions--amount">${amt}</td>
-    <td class="all_transactions--status">Pending</td>
-    <td class="all_transactions--date">25/04/2024</td>
-    <td class="all_transactions--actions">...</td>
+    <td class="all_transactions--status">${status}</td>
+    <td class="all_transactions--date">${25042024}</td>
+    <td class="all_transactions--actions">${"menu"}</td>
   </tr>
-</div>
+
   `;
+
+    containerTransactions.insertAdjacentHTML("afterbegin", html);
   });
 };
-displayAccountTransactions(account1);
+displayAccountTransactions(account2);
 
 let currentAccount;
 
@@ -121,14 +118,3 @@ btnSignin.addEventListener("click", function (e) {
     // Display UI and message
   }
 });
-
-/* <div class="all_transactions">
-  <tr id="all_transactions--row1" class="all_transactions--row2">
-    <td class="all_transactions_type">Expense</td>
-    <td class="all_transactions--details">Fuel Payment</td>
-    <td class="all_transactions--amount">$60</td>
-    <td class="all_transactions--status">Pending</td>
-    <td class="all_transactions--date">25/04/2024</td>
-    <td class="all_transactions--actions">...</td>
-  </tr>
-</div>; */
